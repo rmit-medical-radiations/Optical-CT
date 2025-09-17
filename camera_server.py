02d290f24679694b11dd3264e64af9ec052f8194
Daryl Wilding-McBride <daryl.wilding-mcbride@rmit.edu.au>
2025-09-17 14:03:13 +1000

    Create camera_server.py

---
+++ camera_server.py
@@ -0,0 +1,20 @@
+from flask import Flask, send_file
+import time
+import io
+from picamera2 import Picamera2
+
+app = Flask(__name__)
+
+picam2 = Picamera2()
+picam2.configure(picam2.still_configuration(buffer_count=2))
+picam2.start()
+
+@app.route('/capture')
+def capture():
+    stream = io.BytesIO()
+    picam2.capture_file(stream, format='png')
+    stream.seek(0)
+    return send_file(stream, mimetype='image/png')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
