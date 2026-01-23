6e0b8023adda00328887c2adfd39718a22e49141
Daryl Wilding-McBride <daryl.wilding-mcbride@rmit.edu.au>
2026-01-23 17:24:13 +1100

    Update capture_calibration_shots.py

--- capture_calibration_shots.py
+++ capture_calibration_shots.py
@@ -46,7 +46,7 @@ def take_photo(index: int, crop: dict[str, int] | None = None):
         response.raise_for_status()
 
         img_array = np.frombuffer(response.content, dtype=np.uint8)
-        image = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+        image = cv2.imdecode(img_array, cv2.IMREAD_GRAYSCALE)
 
         if image is None:
             raise ValueError("Failed to decode image")
