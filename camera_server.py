059340f7269929abaef169e0a91202030045966f
Daryl Wilding-McBride <daryl.wilding-mcbride@rmit.edu.au>
2026-01-23 16:37:53 +1100

    Update camera_server.py

--- camera_server.py
+++ camera_server.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 picam2 = Picamera2()
 config = picam2.create_still_configuration(
     main={"format": "YUV420", "size": (512, 512), "preserve_ar": True},
-    raw={"size": picam2.sensor_resolution},
+    raw={"size": (1332, 990)},
     buffer_count=1,
 )
 picam2.configure(config)
