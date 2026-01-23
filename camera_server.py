f51525697df821d2ac098033a6adaa5cb78e368a
Daryl Wilding-McBride <daryl.wilding-mcbride@rmit.edu.au>
2026-01-23 12:39:29 +1100

    Update camera_server.py

--- camera_server.py
+++ camera_server.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 picam2 = Picamera2()
 config = picam2.create_still_configuration(
-    main={"format": "RGB888", "size": (512, 512)},
+    main={"format": "RGB888", "size": (512, 512), "preserve_ar": False},
     buffer_count=1,
 )
 picam2.configure(config)
