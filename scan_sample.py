ab73d95e9d8ba58a48f0711f5e1db47e3d7aaa82
Daryl Wilding-McBride <daryl.wilding-mcbride@rmit.edu.au>
2026-02-27 13:32:08 +1100

    Update scan_sample.py

--- scan_sample.py
+++ scan_sample.py
@@ -235,7 +235,7 @@ with serial.Serial(SERIAL_PORT, BAUDRATE, timeout=TIMEOUT) as ser:
         wait_until_stopped(ser, dn_char=DEVICE_NAME)
 
         # Settle time for vibration/rig flex
-        time.sleep(0.5)
+        time.sleep(1.0)
 
         img = take_photo()
 
