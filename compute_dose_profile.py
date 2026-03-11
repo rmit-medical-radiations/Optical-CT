9b645ae9e1aba9719176c763642aea6e21ac4b22
Daryl Wilding-McBride <daryl.wilding-mcbride@rmit.edu.au>
2026-03-11 16:28:55 +1100

    Update compute_dose_profile.py

--- compute_dose_profile.py
+++ compute_dose_profile.py
@@ -295,7 +295,7 @@ parser.add_argument('--crop_extent', type=positive_int, default=700, help='Recon
 parser.add_argument("--new_volume", action="store_true", help="Force computation of a new attenuation volume.")
 parser.add_argument('--depth_sample_top', type=positive_int, default=50, help='Number of pixels from the top of the reconstruction window to the sample ROI.', required=False)
 parser.add_argument('--depth_sample_height', type=positive_int, default=450, help='Vertical height of the sample ROI in pixels.', required=False)
-parser.add_argument("--visualise_crop_only", action="store_true", help="Only load the images and preview the crop for setup purposes.")
+parser.add_argument("--skip_dose_calculations", action="store_true", help="Only load the images and preview the crop for setup purposes.")
 args = parser.parse_args()
 
 
@@ -337,8 +337,8 @@ with t.step("Load PNG stack"):
     img_uint8 = img_norm.astype(np.uint8)
     cv2.imwrite(os.path.join(RECONSTRUCT_DIR, "example_cropped_projection.png"), img_uint8)
 
-if not args.visualise_crop_only:
-    
+if not args.skip_dose_calculations:
+
     with t.step("Calculate line integrals"):
         P = line_integrals_from_png(imgs, dark, flat)  # (A,H,W)
         angles_deg = np.arange(P.shape[0], dtype=np.float32) * 2.0
